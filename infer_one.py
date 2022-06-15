50f7c693ca1b4eee5a2d796d6e73ddb88418915f
youngkon kim <rladudrhs341@gmail.com>
2022-06-15 16:45:14 +0000

    infer_one.py 돌리면서 생긴 결과들

--- infer_one.py
+++ infer_one.py
@@ -25,11 +25,15 @@ org_text.append("그눔 참 숙제 날래 잘했드래요.")
 org_text.append("그 아는 오데를 그리 가려고 하노?")
 org_text.append("지그믄 너무 조려서 집에 가고 싶다.")
 org_text.append('이게 자 나오까 걱정되고 잘 나오면 좋다.')
+org_text.append("안되. 그렇게 돼면 기계가 고장날 수 읻어!.")
+org_text.append("BTS에서 지민 너무 살앙하고 한국가 보고 시퍼요")
+org_text.append("오늘는 날씨가 너무 졷스니다.")
+org_text.append("구멍에 들깨를 너으니 자동으로 기름이 짜여졌다.")
 
 for text in org_text:
     start = time()
     result = inference_one_text(text)
-    print(result)
+    print(f'{text:<30s} => {result:<30s}')
     end = time()
     print('time elapsed:', end - start)
 
